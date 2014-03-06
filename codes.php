ad40af9dcb11c7326e4dd374e27ec023366bc9dd
Adrian Fraguela <hello@adrianfraguela.com>
2014-03-06 22:33:38 +0000

    Initial commit

---
+++ codes.php
@@ -0,0 +1,151 @@
+<?php if($current_conditions_code == 200){
+
+} elseif($current_conditions_code == 201){
+
+} elseif($current_conditions_code == 202){
+
+} elseif($current_conditions_code == 210){
+
+} elseif($current_conditions_code == 211){
+
+} elseif($current_conditions_code == 212){
+
+} elseif($current_conditions_code == 221){
+
+} elseif($current_conditions_code == 230){
+
+} elseif($current_conditions_code == 231){
+
+} elseif($current_conditions_code == 232){
+
+} elseif($current_conditions_code == 300){
+
+} elseif($current_conditions_code == 301){
+
+} elseif($current_conditions_code == 302){
+
+} elseif($current_conditions_code == 310){
+
+} elseif($current_conditions_code == 311){
+
+} elseif($current_conditions_code == 312){
+
+} elseif($current_conditions_code == 313){
+
+} elseif($current_conditions_code == 314){
+
+} elseif($current_conditions_code == 321){
+
+} elseif($current_conditions_code == 500){
+
+} elseif($current_conditions_code == 501){
+
+} elseif($current_conditions_code == 502){
+
+} elseif($current_conditions_code == 503){
+
+} elseif($current_conditions_code == 504){
+
+} elseif($current_conditions_code == 511){
+
+} elseif($current_conditions_code == 520){
+
+} elseif($current_conditions_code == 521){
+
+} elseif($current_conditions_code == 522){
+
+} elseif($current_conditions_code == 531){
+
+} elseif($current_conditions_code == 600){
+
+} elseif($current_conditions_code == 601){
+
+} elseif($current_conditions_code == 602){
+
+} elseif($current_conditions_code == 611){
+
+} elseif($current_conditions_code == 612){
+
+} elseif($current_conditions_code == 615){
+
+} elseif($current_conditions_code == 616){
+
+} elseif($current_conditions_code == 620){
+
+} elseif($current_conditions_code == 621){
+
+} elseif($current_conditions_code == 622){
+
+} elseif($current_conditions_code == 701){
+
+} elseif($current_conditions_code == 711){
+
+} elseif($current_conditions_code == 721){
+
+} elseif($current_conditions_code == 731){
+
+} elseif($current_conditions_code == 741){
+
+} elseif($current_conditions_code == 751){
+
+} elseif($current_conditions_code == 761){
+
+} elseif($current_conditions_code == 762){
+
+} elseif($current_conditions_code == 771){
+
+} elseif($current_conditions_code == 781){
+
+} elseif($current_conditions_code == 800){
+
+} elseif($current_conditions_code == 801){
+
+} elseif($current_conditions_code == 802){
+
+} elseif($current_conditions_code == 803){
+
+} elseif($current_conditions_code == 804){
+
+} elseif($current_conditions_code == 900){
+
+} elseif($current_conditions_code == 901){
+
+} elseif($current_conditions_code == 902){
+
+} elseif($current_conditions_code == 903){
+
+} elseif($current_conditions_code == 903){
+
+} elseif($current_conditions_code == 904){
+
+} elseif($current_conditions_code == 905){
+
+} elseif($current_conditions_code == 906){
+
+} elseif($current_conditions_code == 950){
+
+} elseif($current_conditions_code == 951){
+
+} elseif($current_conditions_code == 952){
+
+} elseif($current_conditions_code == 953){
+
+} elseif($current_conditions_code == 954){
+
+} elseif($current_conditions_code == 955){
+
+} elseif($current_conditions_code == 956){
+
+} elseif($current_conditions_code == 957){
+
+} elseif($current_conditions_code == 958){
+
+} elseif($current_conditions_code == 959){
+
+} elseif($current_conditions_code == 960){
+
+} elseif($current_conditions_code == 961){
+
+} elseif($current_conditions_code == 962){
+
+}
