2bd26da677b0aee1b391925f9b1a92e799339b07
Adrian Fraguela <hello@adrianfraguela.com>
2014-03-06 22:51:45 +0000

    Added definitions of codes in comments inside statements

--- codes.php
+++ codes.php
@@ -1,155 +1,226 @@
 <?php 
 
 //where $current_conditions_code is the code you get back from your call to the API 
-
 if($current_conditions_code == 200){
+	//thunderstorm with light rain
 
 } elseif($current_conditions_code == 201){
+	//thunderstorm with rain	
 
 } elseif($current_conditions_code == 202){
+	//thunderstorm with heavy rain	
 
 } elseif($current_conditions_code == 210){
+	//light thunderstorm	
 
 } elseif($current_conditions_code == 211){
+	//thunderstorm	
 
 } elseif($current_conditions_code == 212){
+	//heavy thunderstorm	
 
 } elseif($current_conditions_code == 221){
+	//ragged thunderstorm	
 
 } elseif($current_conditions_code == 230){
+	//thunderstorm with light drizzle	
 
 } elseif($current_conditions_code == 231){
+	//thunderstorm with drizzle	
 
 } elseif($current_conditions_code == 232){
+	//thunderstorm with heavy drizzle	
 
 } elseif($current_conditions_code == 300){
+	//light intensity drizzle	
 
 } elseif($current_conditions_code == 301){
+	//drizzle
 
 } elseif($current_conditions_code == 302){
+	//heavy intensity drizzle	
 
 } elseif($current_conditions_code == 310){
+	//light intensity drizzle rain	
 
 } elseif($current_conditions_code == 311){
+	//drizzle rain	
 
 } elseif($current_conditions_code == 312){
+	//heavy intensity drizzle rain	
 
 } elseif($current_conditions_code == 313){
+	//shower rain and drizzle	
 
 } elseif($current_conditions_code == 314){
+	//heavy shower rain and drizzle	
 
 } elseif($current_conditions_code == 321){
+	//shower drizzle	
 
 } elseif($current_conditions_code == 500){
+	//light rain	
 
 } elseif($current_conditions_code == 501){
+	//moderate rain	
 
 } elseif($current_conditions_code == 502){
+	//heavy intensity rain	
 
 } elseif($current_conditions_code == 503){
+	//very heavy rain	
 
 } elseif($current_conditions_code == 504){
+	//extreme rain	
 
 } elseif($current_conditions_code == 511){
+	//freezing rain	
 
 } elseif($current_conditions_code == 520){
+	//light intensity shower rain	
 
 } elseif($current_conditions_code == 521){
+	//shower rain	
 
 } elseif($current_conditions_code == 522){
+	//heavy intensity shower rain	
 
 } elseif($current_conditions_code == 531){
+	//ragged shower rain	
 
 } elseif($current_conditions_code == 600){
+	//light snow	
 
 } elseif($current_conditions_code == 601){
+	//snow
 
 } elseif($current_conditions_code == 602){
+	//heavy snow	
 
 } elseif($current_conditions_code == 611){
+	//sleet
 
 } elseif($current_conditions_code == 612){
+	//shower sleet	
 
 } elseif($current_conditions_code == 615){
+	//light rain and snow	
 
 } elseif($current_conditions_code == 616){
+	//rain and snow	
 
 } elseif($current_conditions_code == 620){
+	//light shower snow	
 
 } elseif($current_conditions_code == 621){
+	//shower snow	
 
 } elseif($current_conditions_code == 622){
+	//heavy shower snow	
 
 } elseif($current_conditions_code == 701){
+	//mist
 
 } elseif($current_conditions_code == 711){
+	//smoke
 
 } elseif($current_conditions_code == 721){
+	//haze
 
 } elseif($current_conditions_code == 731){
+	//Sand/Dust Whirls	
 
 } elseif($current_conditions_code == 741){
+	//Fog
 
 } elseif($current_conditions_code == 751){
+	//sand
 
 } elseif($current_conditions_code == 761){
+	//dust
 
 } elseif($current_conditions_code == 762){
+	//VOLCANIC ASH	
 
 } elseif($current_conditions_code == 771){
+	//SQUALLS
 
 } elseif($current_conditions_code == 781){
+	//TORNADO
 
 } elseif($current_conditions_code == 800){
+	//sky is clear	
 
 } elseif($current_conditions_code == 801){
+	//few clouds	
 
 } elseif($current_conditions_code == 802){
+	//scattered clouds	
 
 } elseif($current_conditions_code == 803){
+	//broken clouds	
 
 } elseif($current_conditions_code == 804){
+	//overcast clouds	
 
 } elseif($current_conditions_code == 900){
+	//tornado
 
 } elseif($current_conditions_code == 901){
+	//tropical storm
 
 } elseif($current_conditions_code == 902){
+	//hurricane
 
 } elseif($current_conditions_code == 903){
-
-} elseif($current_conditions_code == 903){
+	//cold
 
 } elseif($current_conditions_code == 904){
+	//hot
 
 } elseif($current_conditions_code == 905){
+	//windy
 
 } elseif($current_conditions_code == 906){
+	//hail
 
 } elseif($current_conditions_code == 950){
+	//Setting
 
 } elseif($current_conditions_code == 951){
+	//Calm
 
 } elseif($current_conditions_code == 952){
+	//Light breeze
 
 } elseif($current_conditions_code == 953){
+	//Gentle Breeze
 
 } elseif($current_conditions_code == 954){
+	//Moderate breeze
 
 } elseif($current_conditions_code == 955){
+	//Fresh Breeze
 
 } elseif($current_conditions_code == 956){
+	//Strong breeze
 
 } elseif($current_conditions_code == 957){
+	//High wind, near gale
 
 } elseif($current_conditions_code == 958){
+	//Gale
 
 } elseif($current_conditions_code == 959){
+	//Severe Gale
 
 } elseif($current_conditions_code == 960){
+	//Storm
 
 } elseif($current_conditions_code == 961){
+	//Violent Storm
 
 } elseif($current_conditions_code == 962){
+	//Hurricane
 
 }
